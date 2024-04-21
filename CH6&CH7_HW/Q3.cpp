3f5537f18477421db3424c4080ccc588eb8d1aa1
novbo1 <154607823+novbo1@users.noreply.github.com>
2024-04-21 17:02:34 +0800

    Update Q3.cpp

--- CH6&CH7_HW/Q3.cpp
+++ CH6&CH7_HW/Q3.cpp
@@ -6,13 +6,14 @@ create function return stored weight in pounds kilograms ounces
 
 #include <iostream>
 using namespace std;
-
+//define a class Weight
 class Weight {
 public:
+    //Initilize
     void setWeightPounds(double pounds);
     void setWeightKilograms(double kilograms);
     void setWeightOunces(double ounces);
-
+    //Convert
     double getWeightPounds() const;
     double getWeightKilograms() const;
     double getWeightOunces() const;
@@ -20,30 +21,6 @@ public:
 private:
     double pounds;
 };
-
-void Weight::setWeightPounds(double pounds) {
-    this->pounds = pounds;
-}
-
-void Weight::setWeightKilograms(double kilograms) {
-    this->pounds = kilograms * 2.21; // Convert kilograms to pounds
-}
-
-void Weight::setWeightOunces(double ounces) {
-    this->pounds = ounces / 16.0; // Convert ounces to pounds
-}
-
-double Weight::getWeightPounds() const {
-    return pounds;
-}
-
-double Weight::getWeightKilograms() const {
-    return pounds / 2.21; // Convert pounds to kilograms
-}
-
-double Weight::getWeightOunces() const {
-    return pounds * 16.0; // Convert pounds to ounces
-}
 
 int main() {
     Weight weight;
@@ -67,4 +44,34 @@ int main() {
     cout << "Weight in pounds: " << weight.getWeightPounds() << " lbs" << endl;
 
     return 0;
+}
+
+void Weight::setWeightPounds(double pounds) 
+{
+    this->pounds = pounds;
+}
+
+void Weight::setWeightKilograms(double kilograms)
+{
+    this->pounds = kilograms * 2.21; // Convert kilograms to pounds
+}
+
+void Weight::setWeightOunces(double ounces) 
+{
+    this->pounds = ounces / 16.0; // Convert ounces to pounds
+}
+
+double Weight::getWeightPounds() const 
+{
+    return pounds;
+}
+
+double Weight::getWeightKilograms() const 
+{
+    return pounds / 2.21; // Convert pounds to kilograms
+}
+
+double Weight::getWeightOunces() const 
+{
+    return pounds * 16.0; // Convert pounds to ounces
 }
