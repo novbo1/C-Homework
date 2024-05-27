72b7b3a5fad2746b4c9105b382e786f161b1d193
novbo1 <154607823+novbo1@users.noreply.github.com>
2024-05-27 17:10:34 +0800

    Q2 upload

---
+++ CH14&CH15_HW/Q2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class UniverstiyStaff
+{
+public:
+	UniverstiyStaff();
+	UniverstiyStaff(string theName);
+	UniverstiyStaff(const UniverstiyStaff& theObject);
+	string getName() const;
+	UniverstiyStaff& operator= (const UniverstiyStaff& rtSide);
+
+	friend istream& operator>>(istream& inStream, UniverstiyStaff& staffObject);
+	friend ostream& operator>>(ostream& outStream, const UniverstiyStaff& staffObject);
+
+private:
+	string name;
+};
+
+class Student : public UniverstiyStaff
+{
+public:
+private:
+	string universityName;
+	int registrationNumber;
+	UniverstiyStaff proctor;
+};
+
+class ScienceStudent : public Student
+{
+private:
+public:
+	string discipline;
+	string postgraduateCourse;
+}
+
+int main()
+{
+
+}
