3148ab7e307171466a70f11e5ff435d19cae3049
novbo1 <154607823+novbo1@users.noreply.github.com>
2024-06-03 16:28:41 +0800

    Q3 update

--- CH14&CH15_HW/Q3/Q3.cpp
+++ CH14&CH15_HW/Q3/Q3.cpp
@@ -1,114 +1,132 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
-#include <string>
 
 using namespace std;
 
 namespace cs_creature {
 
-    class Creature {
+    class creature {
     private:
-        int strength;           // how much damage this creature inflicts
-        int hitpoints;          // how much damage this creature can sustain
+        int strength;
+        int hitpoints;
 
     public:
-        Creature() : strength(10), hitpoints(10) {}
-
-        Creature(int newStrength, int newHitpoints)
-            : strength(newStrength), hitpoints(newHitpoints) {}
+        creature() : strength(10), hitpoints(10) {}
+        creature(int newStrength, int newHitpoints) : strength(newStrength), hitpoints(newHitpoints) {}
 
         int getStrength() const { return strength; }
         int getHitpoints() const { return hitpoints; }
+        void setStrength(int newStrength) { strength = newStrength; }
+        void setHitpoints(int newHitpoints) { hitpoints = newHitpoints; }
+
+        string getSpecies() const { return "Creature"; }
 
         int getDamage() const {
             int damage = (rand() % strength) + 1;
-            cout << getSpecies() << " attacks for " << damage << " points!" << endl;
-            return damage;
-        }
-
-        virtual string getSpecies() const { return "Creature"; }
-    };
-
-    class Human : public Creature {
-    public:
-        Human() : Creature() {}
-        Human(int newStrength, int newHitpoints) : Creature(newStrength, newHitpoints) {}
-
-        string getSpecies() const { return "Human"; }
-    };
-
-    class Elf : public Creature {
-    public:
-        Elf() : Creature() {}
-        Elf(int newStrength, int newHitpoints) : Creature(newStrength, newHitpoints) {}
-
-        string getSpecies() const { return "Elf"; }
-
-        int getDamage() const {
-            int damage = Creature::getDamage();
-            if ((rand() % 2) == 0) {
-                cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
-                damage *= 2;
-            }
             return damage;
         }
     };
 
-    class Demon : public Creature {
+    class demon : public creature {
     public:
-        Demon() : Creature() {}
-        Demon(int newStrength, int newHitpoints) : Creature(newStrength, newHitpoints) {}
-
-        string getSpecies() const { return "Demon"; }
+        demon() : creature() {}
+        demon(int newStrength, int newHitpoints) : creature(newStrength, newHitpoints) {}
 
         int getDamage() const {
-            int damage = Creature::getDamage();
+            int damage = creature::getDamage();
+            cout << " attacks for " << damage << " points!" << endl;
+
             if (rand() % 4 == 0) {
                 damage += 50;
                 cout << "Demonic attack inflicts 50 additional damage points!" << endl;
             }
+
             return damage;
         }
     };
 
-    class Cyberdemon : public Demon {
+    class human : public creature {
     public:
-        Cyberdemon() : Demon() {}
-        Cyberdemon(int newStrength, int newHitpoints) : Demon(newStrength, newHitpoints) {}
+        human() : creature() {}
+        human(int newStrength, int newHitpoints) : creature(newStrength, newHitpoints) {}
 
-        string getSpecies() const { return "Cyberdemon"; }
+        string getSpecies() const { return "Human"; }
+
+        int getDamage() const {
+            int damage = creature::getDamage();
+            cout << "The human";
+            damage = creature::getDamage();
+            cout << " attacks for " << damage << " points!" << endl;
+            return damage;
+        }
     };
 
-    class Balrog : public Demon {
+    class elf : public creature {
     public:
-        Balrog() : Demon() {}
-        Balrog(int newStrength, int newHitpoints) : Demon(newStrength, newHitpoints) {}
+        elf() : creature() {}
+        elf(int newStrength, int newHitpoints) : creature(newStrength, newHitpoints) {}
+
+        string getSpecies() const { return "Elf"; }
+
+        int getDamage() const {
+            int damage = creature::getDamage();
+            cout << "The elf";
+            if (rand() % 2 == 0) {
+                cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
+                damage *= 2;
+            }
+            cout << " attacks for " << damage << " points!" << endl;
+            return damage;
+        }
+    };
+
+    class cyberdemon : public demon {
+    public:
+        cyberdemon() : demon() {}
+        cyberdemon(int newStrength, int newHitpoints) : demon(newStrength, newHitpoints) {}
+
+        string getSpecies() const { return "Cyberdemon"; }
+
+        int getDamage() const {
+            cout << "The cyberdemon";
+            return demon::getDamage();
+        }
+    };
+
+    class balrog : public demon {
+    public:
+        balrog() : demon() {}
+        balrog(int newStrength, int newHitpoints) : demon(newStrength, newHitpoints) {}
 
         string getSpecies() const { return "Balrog"; }
 
         int getDamage() const {
-            int damage = Demon::getDamage();
+            cout << "The balrog";
+            int damage = demon::getDamage();
             int damage2 = (rand() % getStrength()) + 1;
             cout << "Balrog speed attack inflicts " << damage2 << " additional damage points!" << endl;
-            return damage + damage2;
+            damage += damage2;
+            return damage;
         }
     };
-} // namespace cs_creature
+
+}
 
 int main() {
-    using namespace cs_creature;
-    srand(static_cast<unsigned int>(time(0)));
+    srand(time(0));
 
-    Human h1;
-    Elf e1;
-    Cyberdemon c1;
-    Balrog b1;
+    cs_creature::human h1;
+    cs_creature::elf e1;
+    cs_creature::cyberdemon c1;
+    cs_creature::balrog b1;
 
-    Human h(20, 30);
-    Elf e(40, 50);
-    Cyberdemon c(60, 70);
-    Balrog b(80, 90);
+    cs_creature::human h(20, 30);
+    cs_creature::elf e(40, 50);
+    cs_creature::cyberdemon c(60, 70);
+    cs_creature::balrog b(80, 90);
+
 
     cout << "default human strength/hitpoints: " << h1.getStrength() << "/" << h1.getHitpoints() << endl;
     cout << "default elf strength/hitpoints: " << e1.getStrength() << "/" << e1.getHitpoints() << endl;
@@ -116,30 +134,41 @@ int main() {
     cout << "default balrog strength/hitpoints: " << b1.getStrength() << "/" << b1.getHitpoints() << endl;
     cout << "non-default human strength/hitpoints: " << h.getStrength() << "/" << h.getHitpoints() << endl;
     cout << "non-default elf strength/hitpoints: " << e.getStrength() << "/" << e.getHitpoints() << endl;
+    cout << "non-default cyberdemon strength/hitpoints: " << c.getStrength() << "/" << c.getHitpoints() << endl;
+    cout << "non-default balrog strength/hitpoints: " << b.getStrength() << "/" << b.getHitpoints() << endl;
+    cout << endl << endl;
 
-    cout << "Examples of human damage: " << endl;
+    cout << "Examples of " << h.getSpecies() << " damage: " << endl;
     for (int i = 0; i < 10; i++) {
         int damage = h.getDamage();
-        cout << " Total damage = " << damage << endl << endl;
+        cout << " Total damage = " << damage << endl;
+        cout << endl;
     }
+    cout << endl;
 
-    cout << "Examples of elf damage: " << endl;
+    cout << "Examples of " << e.getSpecies() << " damage: " << endl;
     for (int i = 0; i < 10; i++) {
         int damage = e.getDamage();
-        cout << " Total damage = " << damage << endl << endl;
+        cout << " Total damage = " << damage << endl;
+        cout << endl;
     }
+    cout << endl;
 
-    cout << "Examples of cyberdemon damage: " << endl;
+    cout << "Examples of " << c.getSpecies() << " damage: " << endl;
     for (int i = 0; i < 10; i++) {
         int damage = c.getDamage();
-        cout << " Total damage = " << damage << endl << endl;
+        cout << " Total damage = " << damage << endl;
+        cout << endl;
     }
+    cout << endl;
 
-    cout << "Examples of balrog damage: " << endl;
+    cout << "Examples of " << b.getSpecies() << " damage: " << endl;
     for (int i = 0; i < 10; i++) {
         int damage = b.getDamage();
-        cout << " Total damage = " << damage << endl << endl;
+        cout << " Total damage = " << damage << endl;
+        cout << endl;
     }
+    cout << endl;
 
     return 0;
 }
